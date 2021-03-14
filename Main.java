184d453229956e116caa6e0953c337aa452f3f1d
Ryan Hankins <rhankins@gmail.com>
2021-03-14 08:49:43 -0500

    Increase steps to 10000

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
       for (int i = 0; i < nthreads; i++) {
          threads[i] = new Thread(s.getHead(), i);
       }
-      for (int i = 0; i < 1000; i++) {
+      for (int i = 0; i < 10000; i++) {
 	 for (int j = 0; j < nthreads; j++) {
             threads[j].step();
 	    if (threads[j].betweenMethods()) {
